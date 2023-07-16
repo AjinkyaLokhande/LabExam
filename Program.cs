29d11d8ee7a371d25319a2de8052f2122d42e66a
Ajinkya Lokhande <132905500+AjinkyaLokhande@users.noreply.github.com>
2023-07-16 13:13:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LabExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                WriteToTextFile();
+                Console.WriteLine("Here is the content of the file mytest.txt:");
+                ReadFromTextFile();
+            }
+
+            static void WriteToTextFile()
+            {
+                // Path to the text file
+                StreamWriter writer = File.CreateText("D:\\mytest.txt");
+                writer.WriteLine("Hello and Welcome");
+                writer.WriteLine("It is the first content");
+                writer.WriteLine("Of the text file mytest.txt");
+                writer.Close();
+            }
+
+            static void ReadFromTextFile()
+            {
+                string? s;
+                StreamReader reader = File.OpenText("D:\\mytest.txt");
+                while ((s = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+                reader.Close();
+
+            }
+        }
+    }
+}
